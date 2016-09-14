f3352680324b4cc9f3a866600cc4f578a2f1875c
EMCTeamIR <EMCTeamIR@users.noreply.github.com>
2016-09-14 09:32:50 +0430

    Update write.lua

--- plugins/write.lua
+++ plugins/write.lua
@@ -1,9 +1,9 @@
 local function run(msg, matches)
 	if #matches < 2 then
-		return "بعد از این دستور، با قید یک فاصله کلمه یا جمله ی مورد نظر را جهت زیبا نویسی وارد کنید"
+		return ""
 	end
 	if string.len(matches[2]) > 20 then
-		return "حداکثر حروف مجاز 20 کاراکتر انگلیسی و عدد است"
+		return "Max word Error"
 	end
 	local font_base = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,0,9,8,7,6,5,4,3,2,1,.,_"
 	local font_hash = "z,y,x,w,v,u,t,s,r,q,p,o,n,m,l,k,j,i,h,g,f,e,d,c,b,a,Z,Y,X,W,V,U,T,S,R,Q,P,O,N,M,L,K,J,I,H,G,F,E,D,C,B,A,0,1,2,3,4,5,6,7,8,9,.,_"
@@ -180,7 +180,7 @@ local function run(msg, matches)
 
 		table.insert(result, text)
 	end
-	local result_text = "کلمه ی اولیه: "..matches[2].."\nطراحی با "..tostring(#fonts).." فونت:\n ________________________\n\n "
+	local result_text = ""..matches[2].."\n..tostring(#fonts).." Font "
 	a=0
 	for v=1,#result do
 		a=a+1
@@ -194,8 +194,8 @@ return {
 	usagehtm = '<tr><td align="center">write متن</td><td align="right">با استفاده از این پلاگین میتوانید متون خود را با فونت های متنوع و زیبایی طراحی کنید. حد اکثر کاراکتر های مجاز 20 عدد میباشد و فقط میتوانید از حروف انگلیسی و اعداد استفاده کنید</td></tr>',
 	usage = {"write [text] : زیبا نویسی",},
 	patterns = {
-		"^([Ww]rite) (.*)",
-		"^([Ww]rite)$",
+		"^[#!/]([Ww]rite) (.*)",
+		"^[#!/]([Ww]rite)$",
 		},
 	run = run
 }
